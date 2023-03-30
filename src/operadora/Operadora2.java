21f15f8dcf9dfbfffea1ca8c8bba4af9b12451a9
Henrique-182 <henriqueaugustolobo.dev@gmail.com>
2023-03-30 08:11:00 -0300

    Commit txt
    Saldo, plano ativo e área de trabalho em txt

--- src/operadora/Operadora2.java
+++ src/operadora/Operadora2.java
@@ -1,79 +1,130 @@
 package operadora;
 
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Operadora2 {
 	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		Scanner entradaUsuario = new Scanner(System.in);
+		
+		String arquivoAreaDeTrabalho = "C:\\Users\\henri\\OneDrive\\Área de Trabalho";	
+		
+		Scanner arquivoPlanoAtivo = new Scanner(new FileInputStream(arquivoAreaDeTrabalho + "\\projeto_integrador\\src\\operadora\\planoAtivo.txt"));
+		String planoAtivo = arquivoPlanoAtivo.nextLine();
+		
+		Scanner arquivoSaldo = new Scanner(new FileInputStream(arquivoAreaDeTrabalho + "\\projeto_integrador\\src\\operadora\\saldo.txt"));
+		double saldo = arquivoSaldo.nextDouble();
+		
+		double consumo = 0;
+		List<String> planos = Arrays.asList(" 1GB de internet - R$5,00", 
+											" 2GB de internet - R$10,00", 
+											" 3GB de internet - R$15,00", 
+											" 5GB de internet - R$20,00", 
+											"10GB de internet - R$30,00"
+											);
 		
 		boolean condicao = true; 
 		
 		while (condicao) {
 			System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-			System.out.println(" 1: Consumo de internet");
-			System.out.println(" 2: Planos ativos");
-			System.out.println(" 3: Outros planos");
+			System.out.println(" 1: Consultas");
+			System.out.println(" 2: Compra/Troca");
+			System.out.println(" 3: Cancelamento");
 			System.out.println("-1: Sair");
 			System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 			
 			System.out.print("Opção: ");
-			String opcao = scanner.next();
+			String opcao = entradaUsuario.next();
 			
 			switch(opcao) {
 				case "1":
-					System.out.println("consumo...");
-			        // ---------------------------------------------------------------
+					System.out.println("=-=-=-=-= CONSULTAS =-=-=-=-=");
 			        System.out.println("1 - voltar");
-			        System.out.println("2 - sair");
-			        opcao = scanner.next();
+			        System.out.println("2 - consumo de internet");
+			        System.out.println("3 - saldo");
+			        System.out.println("4 - plano ativo");
+			        System.out.println("5 - outros planos");
+			        System.out.println("6 - sair");
+			        opcao = entradaUsuario.next();
 			        switch (opcao) {
 			            case "1":
-			              break;
+			                break;
 			            case "2":
-			              condicao = false;
-			              System.out.println("Saindo...");
-			              break;
+			            	System.out.println("Consumo: " + consumo + "GB");
+			            	break;
+			            case "3":
+			            	System.out.println("Saldo: R$" + saldo);
+			            	break;
+			            case "4":
+			            	System.out.println(planoAtivo);
+			            	break;
+			            case "5":
+			            	for (String plano : planos) {
+								if (!plano.equals(planoAtivo)) {
+									System.out.println(plano);
+								}
+							}
+			            	break;
+			            case "6":
+			                condicao = false;
+			                System.out.println("Saindo...");
+			                break;
 			            default:
-			              System.out.println("Opção inválida");
+			                System.out.println("Opção inválida");
 			        }
-			        // -----------------------------------------------------------------
 			        break;
 				case "2":
-					System.out.println("ativos...");
-			        // -----------------------------------------------------------------
+					System.out.println("=-=-=-=- COMPRA/TROCA =-=-=-=");
 			        System.out.println("1 - voltar");
-			        System.out.println("2 - sair");
-			        opcao = scanner.next();
+			        System.out.println("2 - comprar");
+			        System.out.println("3 - trocar");
+			        System.out.println("4 - sair");
+			        opcao = entradaUsuario.next();
 			        switch (opcao) {
 			            case "1":
 			                break;
 			            case "2":
+			            	System.out.println("lógica da compra a ser implementada");
+			            	break;
+			            case "3":
+			            	System.out.println("lógica da troca a ser implementada");
+			            	break;
+			            case "4":
 			                condicao = false;
 			                System.out.println("Saindo...");
 			                break;
 			            default:
 			                System.out.println("Opção inválida");
 			        }
-			        // -----------------------------------------------------------------
 			        break;
 				case "3":
-					System.out.println("outros...");
-			          // -----------------------------------------------------------------
+					System.out.println("=-=-=-=- CANCELAMENTO =-=-=-=");
 			        System.out.println("1 - voltar");
-			        System.out.println("2 - sair");
-			        opcao = scanner.next();
+			        System.out.println("2 - cancelar");
+			        System.out.println("3 - sair");
+			        opcao = entradaUsuario.next();
 			        switch (opcao) {
 			            case "1":
 			                break;
 			            case "2":
+			            	System.out.print("Deseja realmente cancelar? [S/N] ");
+			            	String cancela = entradaUsuario.next();
+			            	if (cancela == "S" || cancela == "s") {
+								System.out.println("lógica de cancelamento a ser criada...");
+							} else {
+								System.out.println("Cancelamento não foi efetuado");
+							}
+			            	break;
+			            case "3":
 			                condicao = false;
 			                System.out.println("Saindo...");
 			                break;
 			            default:
 			                System.out.println("Opção inválida");
 			        }
-			        // -----------------------------------------------------------------
 			        break;
 				case "-1":
 					condicao = false;
@@ -83,6 +134,8 @@ public class Operadora2 {
 					System.out.println("Opção inválida");
 			}
 		}
-		scanner.close();
+		entradaUsuario.close();
+		arquivoPlanoAtivo.close();
+		arquivoSaldo.close();
 	}
 }
